652f2890044227dcf3e0ea840bf3cfe5f86809d6
ozkanky <kayazokanbk@gmail.com>
2024-03-21 22:00:50 +0300

    up

--- PersonnelAPI/src/controllers/department.controller.js
+++ PersonnelAPI/src/controllers/department.controller.js
@@ -1,4 +1,49 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    // const data = await Department.find(search)
+    //   .sort(sort)
+    //   .skip(skip)
+    //   .limit(limit);
+    //replace:
+    const data = await res.getModelList(Department);
+
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Department),
+      data,
+    });
+  },
+  create: async (req, res) => {
+const data =await Department.create(req.body)
+
+res.status(201).send({
+    error:false,
+    data
+})
+
+  },
+  read: async (req, res) => {
+    const data = await Department.findOne({_id:req.params.id})
+
+    res.status(200).send({
+        error:false,
+        data
+    })
+  },
+  update: async (req, res) => {
+    const data = await Department.updateOne({_id:req.params.id},req.body,{runValidators:true})
+     res.status(202).send({
+       error: false,
+       data,
+       new:await Department.findOne({_id:req.params.id})
+     });
+  },
+  delete: async (req, res) => {},
+};
