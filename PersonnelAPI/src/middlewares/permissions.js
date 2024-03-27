1fb9425989a15affa5e97c4a43e86881f572c783
ozkanky <kayazokanbk@gmail.com>
2024-03-27 14:07:24 +0300

    permission added

---
+++ PersonnelAPI/src/middlewares/permissions.js
@@ -0,0 +1,43 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+// Middleware: permissions
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    //,kullanıcı girişini kontrol edilecek,kullanıcı girişi yapılmamışsa isLogin girişe izin vermeyecek
+
+    if (req.user && req.user.isActive) {
+      //,login olduktan sonra da isActive false olabileceği için isActive kontrolü takrardan yapıloyor
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+  isLead: (req, res, next) => {
+    const departmentId = req.params?.id; //,isLead in oldugu byerde departmentId de gelmeli
+
+    if (
+      req.user && //,req.user giriş yapmak zorunda
+        req.user.isActive && //,req.user isActive olmak zorunda
+        (req.user.isAdmin|| //,req.user isAdmin olmak zorunda,değilse devamındaki sorgu
+     ( req.user.isLead && //,req.user isLead olmak zorunda
+        req.user.departmentId == departmentId))
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+};
