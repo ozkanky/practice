a1ef17dfe79c3fcaba7c85b43dd8044f66fdf7f6
ozkanky <kayazokanbk@gmail.com>
2024-03-29 17:26:40 +0300

    swaggerAutogen added

--- PersonnelAPI/index.js
+++ PersonnelAPI/index.js
@@ -28,30 +28,63 @@ require("express-async-errors");
 const { dbConnection } = require("./src/configs/dbConnection");
 dbConnection();
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //?Morgan log kaydı tutmaya yarayan middleware' dir
 //https://expressjs.com/en/resources/middleware/morgan.html
 //https://github.com/expressjs/morgan
 // $ npm i morgan
 
-const morgan = require('morgan')
+const morgan = require("morgan");
 
 // app.use(morgan('combined'))
 // app.use(morgan('common'))
 // app.use(morgan('short'))
-app.use(
-  morgan(
-    'IP=:remote-addr |:date[clf]] | METHOD=:method | URL=:url | STATUS=:status| LENGTH=:res[content-length] | REF=:referrer | AGENT=:user-agent'
-  )
-);
+// app.use(
+//   morgan(
+//     'IP=:remote-addr |:date[clf]] | METHOD=:method | URL=:url | STATUS=:status| LENGTH=:res[content-length] | REF=:referrer | AGENT=:user-agent' ));
+
+//? Write log file :
+//,fs modülü (File System) Node.js'de dosya sistemine erişim sağlayan bir modüldür. Bu modül, dosyaları okuma, yazma, düzenleme, silme ve diğer dosya sistemine ilişkin işlemleri gerçekleştirmenizi sağlar. fs modülü, dosya ve dizinlerle ilgili çeşitli işlemleri yapmanıza olanak tanır ve Node.js uygulamalarının dosya sistemiyle etkileşimde bulunmasını sağlar.
+
+// const fs = require('node:fs')
+// app.use(morgan('combined', {
+//     stream: fs.createWriteStream('./access.log',{flags:'a+'})
+// }))
+
+//? flags https://nodejs.org/api/fs.html#file-system-flags 
+//,flags parametresi, fs.createWriteStream() fonksiyonu ile oluşturulan bir yazma akışı için belirli davranışları belirtmek için kullanılır. Bu parametre, oluşturulan akışın ne tür işlemler gerçekleştireceğini belirler
+//+{flags:'a+'} okuma ve ekleme , yoksa dosyayı oluşturma
 
 
+//! Write log file day by day :
+ const fs =require ('node:fs')
 
+
+ const now= new Date()
+
+ const today=now.toISOString().split('T')[0]        //, toISOString ile stringe çevir ,sonra split ile  aradaki T harfine göre parçala ve bunu index 0 'ıncı ksımını al
+// console.log('TODAY  : ', today);
+
+
+app.use(morgan('combined', {
+    stream: 
+    fs.createWriteStream(`./logs/${today}.log`,{flags:'a+'}) //, logs klasörü altında day by day kayıt olsturdu
+}))
+/* ------------------------------------------------------- */
+//* DOCUMANTATON
+
+// https://swagger-autogen.github.io/docs/
+// $ npm i swagger-autogen //, benim isstemimi tarar ve otomatik olarak  JSON dosyası olusturur
+  // $ npm i swagger-ui-express //,JSON dosyasını görsele dönüşütrür
+  // $ npm i redoc-express
 /* ------------------------------------------------------- */
 // Middlewares:
 
 // Accept JSON:
 app.use(express.json());
+
+// Logging :
+app.use(require('./src/middlewares/logging'))
 
 // SessionsCookies:
 app.use(require("cookie-session")({ secret: process.env.SECRET_KEY }));
@@ -85,7 +118,7 @@ app.use(async (req, res, next) => {
 
 /* ------------------------------------------------------- */
 // Authontication : (Simple Token)
-app.use(require('./src/middlewares/authentication'))
+app.use(require("./src/middlewares/authentication"));
 /* ------------------------------------------------------- */
 // Routes:
 
@@ -96,7 +129,7 @@ app.all("/", (req, res) => {
     message: "Welcome to PERSONNEL API",
     session: req.session,
     isLogin: req.isLogin,
-    user:req.user
+    user: req.user,
   });
 });
 
