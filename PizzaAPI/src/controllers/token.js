12d3dd83f185e5ac3e921421ddf8841b18b745a7
ozkanky <kayazokanbk@gmail.com>
2024-04-01 18:12:54 +0300

    token controlller and router

--- PizzaAPI/src/controllers/token.js
+++ PizzaAPI/src/controllers/token.js
@@ -1,4 +1,83 @@
-"use strict"
+"use strict";
+
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Token Contoller:
+
+const Token = require("../models/token");
+
+module.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.tags = ["Tokens"]
+            #swagger.summary = "List Tokens"
+            #swagger.description = `
+                You can send query with endpoint for filter[], search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
+    const data = await res.getModelList(Token);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Token),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    /*
+            #swagger.tags = ["Tokens"]
+            #swagger.summary = "Create Token"
+        */
+    const data = await Token.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    /*
+            #swagger.tags = ["Tokens"]
+            #swagger.summary = "Get Single Token"
+        */
+    const data = await Token.findOne({ _id: req.params.TokenId });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    /*
+            #swagger.tags = ["Tokens"]
+            #swagger.summary = "Update Token"
+        */
+    const data = await Token.updateOne({ _id: req.params.TokenId }, req.body, {
+      runValidators: true,
+    }); //,Token modelinde  {_id: req.params.TokenId }kaydını  updateOne yap, -> req.body kaydıyla update yap,-> { runValidators: true }eger bir update yapılmışsa validasyonlar uygulanır
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Token.findOne({ _id: req.params.id }),
+      //,kayıt güncellendikten sonra güncel kaydı getir
+    });
+  },
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Tokens"]
+            #swagger.summary = "Delete Token"
+        */
+    const data = await Token.deleteOne({ _id: req.params.TokenId });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
+/*------------------------------------------------------- */
--- PizzaAPI/src/routes/token.js
+++ PizzaAPI/src/routes/token.js
@@ -1,9 +1,22 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
+const router = require("express").Router();
 
+const token = require("../controllers/token");
 /* ------------------------------------------------------- */
-module.exports = router
+// routes/token :
+
+// URL : /tokens
+
+router.route("/").get(token.list).post(token.create);
+
+router
+  .route("/:id")
+  .get(token.read)
+  .put(token.update)
+  .delete(token.delete)
+  .patch(token.update);
+/* ------------------------------------------------------- */
+module.exports = router;
