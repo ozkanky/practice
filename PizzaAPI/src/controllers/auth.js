6d0a74da11ff5177514567718c144a31d734675d
ozkanky <kayazokanbk@gmail.com>
2024-04-01 20:54:07 +0300

    up

--- PizzaAPI/src/controllers/auth.js
+++ PizzaAPI/src/controllers/auth.js
@@ -2,8 +2,7 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-
-// Auth Contoller: //,create update yapmaz --> Login - Logout yapar
+// Auth Controller:
 
 const User = require("../models/user");
 const Token = require("../models/token");
@@ -20,37 +19,42 @@ module.exports = {
                 required: true,
                 schema: {
                     "username": "test",
-                    "password": "Aa12345?",
+                    "password": "aA?123456",
                 }
             }
         */
     //, kullanıcı isterse kullanıcı password yada email ile login olur
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
+
     if ((username || email) && password) {
-      const user = await User.findOne({
-        $or: [{ username }, { email }], //, usename veya email varmı
-      });
+
+      const user = await User.findOne({ $or: [{ username }, { email }] }); //, $or: [{ username }, { email }] usename veya email varmı
+
       if (user && user.password == passwordEncrypt(password)) {
         //,kullanıucının gönderdiigi yalin haldeki sifreyi sifrele ,veritabanındaki sifreyle esitmi kontrol et
-        if (user.isActive) {//,kullanıcı aktif mi kontrol et
+        if (user.isActive) {
+          //,kullanıcı aktif mi kontrol et
+          /* SIMPLE TOKEN */
 
-          //*SIMPLE TOKEN*/
+          let tokenData = await Token.findOne({ userId: user.id }); //,token veritabanında var mı kontrol et
 
-          let tokenData= await Token.findOne({userId:user._id}) //,token veritabanında var mı kontrol et
+          if (!tokenData)
+            tokenData = await Token.create({
+              //,token veritabanında yoksa token olustur
+              userId: user.id, //,kullanıcı id
+              token: passwordEncrypt(user.id + Date.now()), //, token user id + tarih ile olsutur ve encrypt et
+            });
 
-          if(!tokenData){ 
-            tokenData=await Token.create({ //,token veritabanında yoksa token olustur
-                userId :user._id, //,kullanıcı id
-                token:passwordEncrypt((user.id + Date.now()).toString()) //, token user id + tarih ile olsutur ve encrypt et
-            })
-           }
+          /* SIMPLE TOKEN */
 
-          //*SIMPLE TOKEN*/
           res.status(200).send({
-              error: false,
-              token: tokenData.token,
-              user
-          })
+            error: false,
+            token: tokenData.token,
+            user,
+          });
+        } else {
+          res.errorStatusCode = 401;
+          throw new Error("This account is not active.");
         }
       } else {
         res.errorStatusCode = 401;
@@ -61,5 +65,23 @@ module.exports = {
       throw new Error("Please enter username/email and password.");
     }
   },
-  logout: async (req, res) => {},
+
+  logout: async (req, res) => {
+    /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "simpleToken: Logout"
+            #swagger.description = 'Delete token key.'
+        */
+
+    const auth = req.headers?.authorization; // Token ...tokenKey...
+    const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
+    const result = await Token.deleteOne({ token: tokenKey[1] });
+
+    res.send({
+      error: false,
+      message: "Token deleted. Logout was OK.",
+      result,
+    });
+  },
 };
+//
