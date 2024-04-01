bdefa80a555be89fa98ac8897e2c3a34e5c0e68a
ozkanky <kayazokanbk@gmail.com>
2024-04-01 15:14:12 +0300

    userModel added

--- PizzaAPI/src/models/user.js
+++ PizzaAPI/src/models/user.js
@@ -1,6 +1,47 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+//user Model
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users", //,küçük harflerle ve cogul
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+// Model:
+module.exports = mongoose.model("User", UserSchema);
