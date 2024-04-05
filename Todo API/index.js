049771762728c9500c180f314997abd99248189f
ozkanky <kayazokanbk@gmail.com>
2024-04-05 12:23:06 +0300

    db connection is done

--- Todo API/index.js
+++ Todo API/index.js
@@ -3,20 +3,31 @@
     EXPRESSJS - TODO Project with Mongoose
 ------------------------------------------------------- */
 
-const express=require('express')
-app =express()
+const express = require("express");
+const app = express();
 /* ------------------------------------------------------- */
 // Required Modules:
 
-// envVariables to process.env: 
-require('dotenv').config()
-const PORT =process.env.PORT || 8000
+// envVariables to process.env:
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
+
+/* ------------------------------------------------------- */
+// Configrations :
+
+const {dbConnection} = require("./src/configs/dbConnection");
+dbConnection();
+
+/* ------------------------------------------------------- */
+// Middlewares : 
+
 
 
 
 /* ------------------------------------------------------- */
+// RUN SERVER:
 
-/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("http:127.0.0.1:8000" + PORT));
